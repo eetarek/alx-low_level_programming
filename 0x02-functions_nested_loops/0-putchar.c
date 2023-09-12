53f7336c602a96772abaccb15d345ab7ca3253ec
eetarek <emadeldeen.tarek@gmail.com>
2023-09-12 14:38:47 -0700

    0 modify

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main -Entry point
+ * main - Entry point
  * Return:0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -18,3 +17,6 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
+
+
+
