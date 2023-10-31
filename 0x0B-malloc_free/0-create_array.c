1a0bf7683ec47608e4b100bba67b2ec6bf6bc214
eetarek <emadeldeen.tarek@gmail.com>
2023-10-30 17:32:52 -0700

    first task modification

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
-	if (size == 0 || n==0)
+
+	if (size == 0 || n == 0)
 		return (0);
+
 	while (size--)
 		n[size] = c;
 
